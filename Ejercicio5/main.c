fa8167c19120d8a8dd0185b00c66873af2947043
javieralonso-blip <javier_alonso@ceroca.cat>
2026-03-18 12:15:43 +0100

    Update main.c

--- Ejercicio5/main.c
+++ Ejercicio5/main.c
@@ -4,12 +4,10 @@
 int main() {
     double precision, ratio, prev_ratio;
     int a = 1, b = 1, temp, n = 2;
-
     printf("Introduce precision: ");
     scanf("%lf", &precision);
 
     prev_ratio = (double)b / a;
-
     do {
         temp = a + b;
         a = b;
@@ -17,7 +15,6 @@ int main() {
         ratio = (double)b / a;
         n++;
     } while (fabs(ratio - prev_ratio) > precision);
-
     printf("Razon aurea: %.5f\n", ratio);
     printf("Iteraciones: %d\n", n);
 
